4e5967bcbfe6ebeff46c6209daaec40a9ba4374b
Patrick Allen <github.com@eth0.uk.net>
2015-05-31 00:14:49 +0100

    Initial commit

---
+++ functions.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * Change text strings
+ *
+ * @link http://codex.wordpress.org/Plugin_API/Filter_Reference/gettext
+ */
+function hypermin_text_strings( $translated_text, $text, $domain ) {
+	switch ( $translated_text ) {
+		case 'Post Comment' :
+			$translated_text = __( '&lt; Post Comment &gt;', 'hypermin' );
+			break;
+	}
+	return $translated_text;
+}
+add_filter( 'gettext', 'hypermin_text_strings', 20, 3 );
+
+
+
+function hypermin_search_form_modify( $html ) {
+	$html = str_replace(
+		'<input type="search" class="search-field" placeholder="Search &hellip;" value="',
+		'<span class="search-prompt">Search: [<input type="search" class="search-field" placeholder="" value="',
+		$html
+	);
+	return str_replace(
+		'" name="s" title="Search for:" />',
+		'" name="s" title="Search for:" />]</span>',
+		$html
+	);
+}
+add_filter( 'get_search_form', 'hypermin_search_form_modify' );
+
+
+function replace_sidebar_headers() {
+  unregister_sidebar('sidebar-1');
+  /** I have looked for the ID of the sidebar by looking at        
+   *  the source code in the admin.. and saw the widget's id="sidebar-4"
+   */ 
+
+	register_sidebar( array(
+		'name'          => __( 'Widget Area', 'twentyfifteen' ),
+		'id'            => 'sidebar-1',
+		'description'   => __( 'Add widgets here to appear in your sidebar.', 'twentyfifteen' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s"><div class="widget-dialog">',
+		'after_widget'  => '</div></aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'replace_sidebar_headers', 77);
+
+
+function theme_enqueue_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array('parent-style')
+	);
+	wp_enqueue_style( 'google-fonts', 'http://fonts.googleapis.com/css?family=Inconsolata|VT323' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+
+function twentyfifteen_entry_meta() {
+	if ( is_sticky() && is_home() && ! is_paged() ) {
+		printf( '<span class="sticky-post">%s</span>', __( 'Featured', 'twentyfifteen' ) );
+	}
+
+	$format = get_post_format();
+	if ( current_theme_supports( 'post-formats', $format ) ) {
+		printf( '<span class="entry-format">%1$s<a href="%2$s">%3$s</a></span>',
+			sprintf( '<span class="screen-reader-text">%s </span>', _x( 'Format', 'Used before post format.', 'twentyfifteen' ) ),
+			esc_url( get_post_format_link( $format ) ),
+			get_post_format_string( $format )
+		);
+	}
+
+	if ( in_array( get_post_type(), array( 'post', 'attachment' ) ) ) {
+		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		}
+
+		$time_string = sprintf( $time_string,
+			esc_attr( get_the_date( 'c' ) ),
+			get_the_date(),
+			esc_attr( get_the_modified_date( 'c' ) ),
+			get_the_modified_date()
+		);
+
+		printf( '<span class="posted-on"><span class="screen-reader-text">%1$s </span><a href="%2$s" rel="bookmark">%3$s</a></span>',
+			_x( 'Posted on', 'Used before publish date.', 'twentyfifteen' ),
+			esc_url( get_permalink() ),
+			$time_string
+		);
+	}
+
+	if ( 'post' == get_post_type() ) {
+		if ( is_singular() || is_multi_author() ) {
+			printf( '<span class="byline"><span class="author vcard"><span class="screen-reader-text">%1$s </span><a class="url fn n" href="%2$s">%3$s</a></span></span>',
+				_x( 'Author', 'Used before post author name.', 'twentyfifteen' ),
+				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+				get_the_author()
+			);
+		}
+
+		$categories_list = get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfifteen' ) );
+		if ( $categories_list && twentyfifteen_categorized_blog() ) {
+			printf( '<span class="cat-links"><span class="screen-reader-text">%1$s </span>%2$s</span>',
+				_x( 'Categories', 'Used before category names.', 'twentyfifteen' ),
+				$categories_list
+			);
+		}
+
+		$tags_list = get_the_tag_list( '', _x( ' <span class="tag-separator">//</span> ', 'Used between list items, there is a space after the comma.', 'twentyfifteen' ) );
+		if ( $tags_list ) {
+			printf( '<span class="tags-links"><span class="screen-reader-text">%1$s </span>%2$s</span>',
+				_x( 'Tags', 'Used before tag names.', 'twentyfifteen' ),
+				$tags_list
+			);
+		}
+	}
+
+	if ( is_attachment() && wp_attachment_is_image() ) {
+		// Retrieve attachment metadata.
+		$metadata = wp_get_attachment_metadata();
+
+		printf( '<span class="full-size-link"><span class="screen-reader-text">%1$s </span><a href="%2$s">%3$s &times; %4$s</a></span>',
+			_x( 'Full size', 'Used before full size attachment link.', 'twentyfifteen' ),
+			esc_url( wp_get_attachment_url() ),
+			$metadata['width'],
+			$metadata['height']
+		);
+	}
+
+	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		echo '<span class="comments-link">';
+		comments_popup_link( __( 'Leave a comment', 'twentyfifteen' ), __( '1 Comment', 'twentyfifteen' ), __( '% Comments', 'twentyfifteen' ) );
+		echo '</span>';
+	}
+}
+
+
+
+
+?>
